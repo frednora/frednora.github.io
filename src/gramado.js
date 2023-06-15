0c2e6a9e9fcabd15f1ebf77c50b4c37341e9d48e
frednora <freddynora@gmail.com>
2023-06-15 20:41:24 -0300

    webpage: Simple css and simple js.

--- src/gramado.js
+++ src/gramado.js
@@ -1,11 +1,22 @@
 
+let __gramado_status = 0;
 
-
-function gramadoInitialize()
-{
+function gramadoInitialize(){
     let ReturnValue=1234;
-    alert("Initializing Gramado ...");
+    console.log("Initializing Gramado ...")
+    gramadoHelloWorld();
+    __gramado_status = 1;
     return ReturnValue;
 }
+
+function gramadoHelloWorld(){
+    console.log("Hello")
+    if (__gramado_status == 1){
+        window.alert("Initialized");
+    }else{
+        window.alert("Not initialized");
+    }
+}
+
 
 
